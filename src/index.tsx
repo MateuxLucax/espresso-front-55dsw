55bed3675fe805fb3036ba846c8749f22797c865
Mateus Lucas <mateuxlucax@gmail.com>
2023-10-26 15:00:13 -0300

    feat: project init

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './assets/scss/index.scss';
 import reportWebVitals from './reportWebVitals';
+import LandingPage from './LandingPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>
 );
 
