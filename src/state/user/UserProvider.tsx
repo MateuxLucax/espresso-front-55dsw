7e4979d51992c994d89748ab8bb40d12a61db188
Mateus Lucas <mateuxlucax@gmail.com>
2023-11-28 23:19:34 -0300

    feat: add UserProvider

---
+++ src/state/user/UserProvider.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, ReactNode } from "react";
+import UserContext from "./UserContext";
+import { Request } from "../../services/request";
+import { User } from "../../models/User";
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | undefined>(undefined);
+
+  useEffect(() => {
+    async function checkUser() {
+      const currentUser = localStorage.getItem("user");
+      if (!currentUser) return;
+
+      try {
+        const parsedUser = JSON.parse(currentUser) as User;
+        const response = await Request.get("artisan/me", {
+          Authorization: `Bearer ${parsedUser.token}`,
+        });
+        if (!response) return;
+
+        setUser(parsedUser);
+      } catch (error) {
+        console.warn(error);
+        setUser(undefined);
+        localStorage.removeItem("user");
+      }
+    }
+
+    checkUser();
+  }, []);
+
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+};
+
+export default UserProvider;
